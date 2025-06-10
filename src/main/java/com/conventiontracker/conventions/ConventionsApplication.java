230318d891b2b92d3bcbc47ce1b216736bf51749
Alex Smith <alexnsmith92@gmail.com>
2025-06-10 14:24:04 -0400

    Initial commit

---
+++ src/main/java/com/conventiontracker/conventions/ConventionsApplication.java
@@ -0,0 +1,13 @@
+package com.conventiontracker.conventions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConventionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conventiontracker/conventions/ConventionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conventiontracker.conventions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConventionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
